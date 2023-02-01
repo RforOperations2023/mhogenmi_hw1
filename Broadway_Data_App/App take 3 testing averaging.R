b121885064a4383f8c859974e962b25dd8ee434d
morganhogenmiller <123034386+morganhogenmiller@users.noreply.github.com>
2023-01-31 22:34:33 -0500

    Editing column names and cleaning variable names in dataset - editing the theme of the donut plot to make it more minimal

--- Broadway_Data_App/App take 3 testing averaging.R
+++ Broadway_Data_App/App take 3 testing averaging.R
@@ -5,21 +5,39 @@ library(plotly)
 library(lubridate)
 bway <- read.csv("Broadway_Prettified_LFG.csv")
 
+
+bway <- bway %>% 
+  rename( "Show" = "Show.Name",
+          "Date" = "Date.Full",
+          "Year" = "Date.Year",
+          "Month" = "Date.Month",
+          "Theatre" = "Show.Theatre",
+          "Avg_Attendance" = "Statistics.Attendance",
+          "Avg_Capacity" = "Statistics.Capacity",
+          "Avg_Gross" = "Statistics.Gross",
+          "Type" = "Show.Type"
+  )
+
+
 bway_group_month <- bway %>% 
-  group_by(Show.Theatre,Date.Year,Show.Name, Date.Month) %>% 
-  summarise(mean_capacity=mean(Statistics.Capacity),
-            mean_attendance=mean(Statistics.Attendance),
-            mean_gross=mean(Statistics.Gross),
+  group_by(Theatre,Year,Show, Month) %>% 
+  summarise(Capacity=mean(Avg_Capacity),
+            Attendance=mean(Avg_Attendance),
+            Gross=mean(Avg_Gross),
             .groups = 'drop')
 
 
 #TEST TOMORROW --------------------------
 gb_type <- bway %>%
-  group_by(Date.Year, Show.Theatre) %>%
-  count(Show.Type)
+  group_by(Year, Theatre) %>%
+  count(Type)
 gb_type$fraction <- gb_type$n/ sum(gb_type$n)
 gb_type$ymax = cumsum(gb_type$fraction)
 gb_type$ymin = c(0, head(gb_type$ymax, n=-1))
+
+# Compute a good label
+gb_type$labelPosition <- (gb_type$ymax + gb_type$ymin) / 2
+gb_type$label <- paste0(gb_type$Type, "\n (# of weeks): ", gb_type$n)
 
 
 
@@ -36,23 +54,25 @@ gb_type$ymin = c(0, head(gb_type$ymax, n=-1))
 # Choose color by option ----------------------------------
       selectInput(inputId = "z",
             label = "Theatre:",
-            choices = c(unique(bway$Show.Theatre)),
-            selected = "Marquis Theatre"),
+            choices = sort(c(unique(bway$Theatre))),
+            selected = "Walter Kerr"),
 
 
 # Input: Specification of range within an interval ----
-sliderInput(inputId = "Date.Year",
+sliderInput(inputId = "Year",
             label = "Year:",
-            min = 2000, max = 2020,
+            min = 2000, max = 2015,
             step = 1,
             sep = "",
-            value = 2010),
+            value = 2008),
 
 # Select variable for y-axis ----------------------------------
 selectInput(inputId = "y",
             label = "Y-axis (Graph on the Right)",
-            choices = c("mean_capacity", "mean_attendance","mean_gross"),
-            selected = "mean_capacity"),
+            choices = c("Average Capacity Filled (%)" = "Capacity", 
+                        "Average Weekly Total Audience Count" = "Attendance",
+                        "Average Weekly Gross" = "Gross"),
+            selected = "Capacity"),
     
 
 # Show data table ---------------------------------------------
@@ -85,40 +105,43 @@ selectInput(inputId = "y",
    
 # Create a subset of data filtering by Year Range ------
    bway_subset <- reactive({
-     req(input$Date.Year) # ensure availablity of value before proceeding
+     req(input$Year) # ensure availablity of value before proceeding
      bway %>%
-       filter(Date.Year == input$Date.Year,
-              Show.Theatre == input$z)
+       filter(Year == input$Year,
+              Theatre == input$z)
    })
    
 #Make another subset using the code in the test script for grouped by month averages
 
    bway_subset_scatter <- reactive({
-     req(input$Date.Year) # ensure availablity of value before proceeding
+     req(input$Year) # ensure availablity of value before proceeding
      bway_group_month %>%
-       filter(Date.Year == input$Date.Year,
-              Show.Theatre == input$z)
+       filter(Year == input$Year,
+              Theatre == input$z)
    })
    
    
 # #Make another subset using the code in the test script for donut chart for show type
 #    
    bway_subset_donut <- reactive({
-     req(input$Date.Year) # ensure availablity of value before proceeding
+     req(input$Year) # ensure availablity of value before proceeding
      gb_type %>%
-       filter(Date.Year == input$Date.Year,
-              Show.Theatre == input$z)
+       filter(Year == input$Year,
+              Theatre == input$z)
    })
 
 #
 #   # Create scatterplot object the plotOutput function is expecting --
    output$scatterplot <- renderPlotly({
     
-     ggplotly(ggplot(data = bway_subset_scatter(), aes_string(x = "Date.Month", 
+     ggplotly(ggplot(data = bway_subset_scatter(), aes_string(x = "Month", 
                                      y = input$y,
-                                    color = "Show.Name")) +
+                                    color = c("Meh" = "Show" ))) +
       geom_point() +  theme(axis.text.x = element_text(angle = 45)) + 
         scale_x_discrete(limits=c("January", "February","March","April",'May',"June","July","August",'September',"October","November","December"))
+      + ggtitle("Monthly Averages")  + labs(x = "Month", y = input$y) 
+      + scale_fill_discrete(name = "Meh")
+
       
       ) 
      
@@ -128,20 +151,23 @@ selectInput(inputId = "y",
 
        # Create barplot object the plotOutput function is expecting --
      output$barplot <- renderPlot({
-      ggplot(data = bway_subset(), aes_string(x ="Show.Name",
-                                               fill = "Show.Name" 
+      ggplot(data = bway_subset(), aes_string(x ="Show",
+                                               fill = "Show" 
                                               )) +
       geom_bar() + labs(x = "Show", y="Number of Weeks") +
       theme(axis.text.x = element_text(angle = 90))
   
    })
      
-     # PUT THIS IN A RENDERPLOT TOMORROW FOR THE DONUT CHART
+     # Create donut plot object the plotOutput function is expecting --
+     
      output$donut <- renderPlot({
-     ggplot(bway_subset_donut(), aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=Show.Type)) +
+     ggplot(bway_subset_donut(), aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=Type)) +
        geom_rect() +
        coord_polar(theta="y") + # Try to remove that to understand how the chart is built initially
-       xlim(c(2, 4)) # Try to remove that to see how to make a pie chart
+       xlim(c(2, 4))  + 
+       geom_label( x=3.5, aes(y=labelPosition, label=label), size=4) +
+       theme_void()
      })
 
    
@@ -149,7 +175,7 @@ selectInput(inputId = "y",
    # Print data table if checked -------------------------------------
    output$bwaytable <- DT::renderDataTable(
      if(input$show_data){
-       DT::datatable(data = bway_subset()[, 1:6], 
+       DT::datatable(data = bway_subset()[, 2:9], 
                      options = list(pageLength = 10), 
                      rownames = FALSE)
      }
--- Broadway_Data_App/Script_for_testing.R
+++ Broadway_Data_App/Script_for_testing.R
@@ -1,33 +1,65 @@
 
 library(dplyr)
 bway <- read.csv('Broadway_Prettified_LFG.csv', sep = ',', encoding = 'utf-8')
-print(bway)
 str(bway)
+print
 
-library(ggplot2)
-bway_group_type_count <- bway %>% 
-      group_by(Date.Year,Show.Theatre) %>% 
-      count(Show.Type)
-
-gb_type <- bway %>%
-  group_by(Show.Theatre) %>%
-  mutate(count(Show.Type))
-gb_type$fraction <- gb_type$n/ sum(gb_type$n)
-gb_type$ymax = cumsum(gb_type$fraction)
-gb_type$ymin = c(0, head(gb_type$ymax, n=-1))
-
-show(gb_type)
-# Make the plot
-ggplot(gb_type, aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=Show.Type)) +
-  geom_rect() +
-  coord_polar(theta="y") + # Try to remove that to understand how the chart is built initially
-  xlim(c(2, 4)) # Try to remove that to see how to make a pie chart
+bway <- bway%>%
+  select(Show.Theatre,
+         Show.Name,
+         Show.Type,
+         Date.Full,
+         Date.Year,
+         Date.Month,
+         Statistics.Attendance,
+         Statistics.Gross,
+         Statistics.Capacity,
+          )
 
 
-show(gb_type)
+
+bway <- bway %>% 
+  rename( "Show" = "Show.Name",
+          "Date" = "Date.Full",
+          "Year" = "Date.Year",
+          "Month" = "Date.Month",
+          "Theatre" = "Show.Theatre",
+          "Avg_Attendance" = "Statistics.Attendance",
+          "Avg_Capacity" = "Statistics.Capacity",
+          "Avg_Gross" = "Statistics.Gross",
+          "Type" = "Show.Type"
+  )
 
 
-print(bway_group_month)
+
+bway <- bway %>% 
+  rename( "Show" = "Show.Name"
+  )
+
+# library(ggplot2)
+# bway_group_type_count <- bway %>% 
+#       group_by(Date.Year,Show.Theatre) %>% 
+#       count(Show.Type)
+# 
+# gb_type <- bway %>%
+#   group_by(Show.Theatre) %>%
+#   mutate(count(Show.Type))
+# gb_type$fraction <- gb_type$n/ sum(gb_type$n)
+# gb_type$ymax = cumsum(gb_type$fraction)
+# gb_type$ymin = c(0, head(gb_type$ymax, n=-1))
+# 
+# show(gb_type)
+# # Make the plot
+# ggplot(gb_type, aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=Show.Type)) +
+#   geom_rect() +
+#   coord_polar(theta="y") + # Try to remove that to understand how the chart is built initially
+#   xlim(c(2, 4)) # Try to remove that to see how to make a pie chart
+# 
+# 
+# show(gb_type)
+# 
+# 
+# print(bway_group_month)
 
 
 
